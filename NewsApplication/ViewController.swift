e24642d4aa62ec12dfc929cd0108f76b36429892
Veysel Can Bingöl <veyselcanbingol@gmail.com>
2022-06-02 08:54:54 +0300

    Initial Commit

---
+++ NewsApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApplication
+//
+//  Created by Veysel Can Bingöl on 2.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
